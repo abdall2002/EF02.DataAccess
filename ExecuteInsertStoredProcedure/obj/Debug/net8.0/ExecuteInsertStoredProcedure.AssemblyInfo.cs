c6d2a3b11ca9451086535eb0a5d63515829da5e0
abdall2002 <abdallahaliahmed517@gmail.com>
2025-07-10 15:40:59 +0300

    EEE

--- ExecuteInsertStoredProcedure/obj/Debug/net8.0/ExecuteInsertStoredProcedure.AssemblyInfo.cs
+++ ExecuteInsertStoredProcedure/obj/Debug/net8.0/ExecuteInsertStoredProcedure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExecuteInsertStoredProcedure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fc1aae8c54ffa2ada305f6db40c9d5e4922f7c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4f31cf36041a5ee4a9cac1ba3e0999ca764df8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExecuteInsertStoredProcedure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExecuteInsertStoredProcedure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ ExecuteRawSqlDataAdapter/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Data;
+using System.Net.Http.Headers;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+var configuration = new ConfigurationBuilder()
+               .AddJsonFile("appsettings.json")
+               .Build();
+//Console.WriteLine(configuration.GetSection("constr").Value);
+
+SqlConnection conn = new SqlConnection(configuration.GetSection("constr").Value);
+
+conn.Open();
+
+var sql = "SELECT * FROM WALLETS";
+
+SqlDataAdapter adapter = new SqlDataAdapter(sql, conn);
+
+DataTable dt = new DataTable();
+
+adapter.Fill(dt);
+
+conn.Close();
+
+foreach (DataRow dr in dt.Rows)
+{
+    var wallet = new Wallet
+    {
+        Id = Convert.ToInt32(dr["Id"]),
+        Holder = Convert.ToString(dr["Holder"]),
+        Balance = Convert.ToDecimal(dr["Balance"])
+    };
+    Console.WriteLine(wallet);
+}
